b1f11d4d2763aab8e67cba5d11ff0625a9372f31
LeHamzaSh <hrsheikh05@gmail.com>
2021-07-06 21:01:36 +0400

    updated values into new workbook

--- app.py
+++ app.py
@@ -4,5 +4,10 @@ sheet = workbook['Sheet1']
 cell = sheet['a1']
 cell = sheet.cell(1,1)
 
-for row in range(1, sheet.max_row + 1):
-     print(row)
+for row in range(2, sheet.max_row + 1):
+     cell = sheet.cell(row, 3)
+     correct_price = cell.value * 0.9
+     corrected_price_cell = sheet.cell(row, 4)
+     corrected_price_cell.value = correct_price
+
+workbook.save('transactions2.xlsx')
