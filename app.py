dec4ed895c15fc37a1f9852e8c8f6ab3a6b12277
LeHamzaSh <hrsheikh05@gmail.com>
2021-07-06 21:08:02 +0400

    Added a barchart to the excel workbook

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import openpyxl as xl
+from openpyxl.chart import BarChart, Reference
+
 workbook = xl.load_workbook('transactions.xlsx')
 sheet = workbook['Sheet1']
 cell = sheet['a1']
@@ -10,4 +12,8 @@ for row in range(2, sheet.max_row + 1):
      corrected_price_cell = sheet.cell(row, 4)
      corrected_price_cell.value = correct_price
 
+values = Reference(sheet, min_row=2, max_row=sheet.max_row, min_col=4,max_col=4)
+chart = BarChart()
+chart.add_data(values)
+sheet.add_chart(chart, 'e2')
 workbook.save('transactions2.xlsx')
