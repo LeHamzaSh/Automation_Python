aa374ae563e376b617781c8d273b70ca4fd72382
LeHamzaSh <hrsheikh05@gmail.com>
2021-07-06 20:53:01 +0400

    Added for loop

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
 import openpyxl as xl
 workbook = xl.load_workbook('transactions.xlsx')
+sheet = workbook['Sheet1']
+cell = sheet['a1']
+cell = sheet.cell(1,1)
+
+for row in range(1, sheet.max_row + 1):
+     print(row)
